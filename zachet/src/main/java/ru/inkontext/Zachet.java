0209322f9ed0aeb237c29bc5e3a50fa7d5c4f6f4
stolbovd <stolbovd@gmail.com>
2020-12-18 12:28:03 +0500

    Добавил в приложение Зачет аутентификацию с помощью Discord

---
+++ zachet/src/main/java/ru/inkontext/Zachet.java
@@ -0,0 +1,11 @@
+package ru.inkontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zachet {
+	public static void main(String... parameters) {
+		SpringApplication.run(Zachet.class, parameters);
+	}
+}
---
+++ zachet/src/main/java/ru/inkontext/config/OAuth2UserAgentUtils.java
@@ -0,0 +1,16 @@
+package ru.inkontext.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
+
+class OAuth2UserAgentUtils {
+	static RequestEntity<?> withUserAgent(RequestEntity<?> request) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.putAll(request.getHeaders());
+		headers.add(HttpHeaders.USER_AGENT, DISCORD_BOT_USER_AGENT);
+
+		return new RequestEntity<>(request.getBody(), headers, request.getMethod(), request.getUrl());
+	}
+
+	private static final String DISCORD_BOT_USER_AGENT = "DiscordBot (https://github.com/stolbovd/handy-apps/tree/main/zachet)";
+}
